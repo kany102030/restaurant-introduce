97ef0f833db28e2240e5866ba8b0212fd0722743
kany <kany102030@gmail.com>
2022-05-20 22:33:30 +0800

    add dynamic page
    1. load restaurants.json
    2. set route for /search /restaurant
    3. let index page show data in restaurants.json
    4. show detail page when press submit button

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+
+const restaurantList = require('./restaurant.json').results
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 const port = 3000
-app.get('/show', function (req, res) {
-  res.render('show')
+app.get('/search', function (req, res) {
+  const keyword = req.query.keyword
+  selectedRestaurants = restaurantList.filter(function (restaurtant) {
+    return restaurtant.name.toLocaleLowerCase().includes(keyword.trim().toLocaleLowerCase())
+  })
+
+  res.render('index', { selectedRestaurants: selectedRestaurants, keyword: keyword })
 })
+app.get('/restaurants/:id', function (req, res) {
+  const id = Number(req.params.id)
+  restaurant = restaurantList.find(function (restaurant) {
+    return restaurant.id === id
+  })
+  res.render('show', { restaurant: restaurant })
+})
+
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render('index', { selectedRestaurants: restaurantList })
 })
 app.listen(port, function () {
   console.log(`listening on http://localhost:3000`)
